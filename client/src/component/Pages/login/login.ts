c50a62345a54548ffb1dba55616bcd29045c089a
sadishaaaa <sthasadisha2019@gmail.com>
2024-01-11 17:28:37 +0545

    integrated login

---
+++ client/src/component/Pages/login/login.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const signInEndpoint = "http://localhost:8000/auth/login";
+const email = document.getElementById("signInEmail") as HTMLInputElement;
+const password = document.getElementById("signInPassword") as HTMLInputElement;
+const signInButton = document.getElementById(
+  "signInButton"
+) as HTMLButtonElement;
+
+signInButton.addEventListener("click", async (event) => {
+  try {
+    event.preventDefault();
+    const userData = {
+      email: email.value,
+      password: password.value,
+    };
+    const response = await axios.post(signInEndpoint, userData);
+    localStorage.setItem("accessToken", response.data.accessToken);
+    localStorage.setItem("refreshToken", response.data.refreshToken);
+    const userRole = response.data.role;
+    console.log(userRole);
+    if (userRole === "user") {
+      window.location.href = "/component/Pages/home.html";
+    } else if (userRole === "admin") {
+      window.location.href = "/component/Sidebar/sidebar.html";
+    }
+
+    console.log("login successfully", response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ client/src/utils/checkAuth.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const checkAuth = async () => {
+  const accessToken = localStorage.getItem("accessToken") || "";
+  // const refreshToken = localStorage.getItem("refreshToken") || "";
+  if (accessToken !== "") {
+    const response = await axios.get("http://localhost:8000/users/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log(response.data.data);
+    return response.data.data;
+  } else {
+    window.location.href = "../component/Pages/login/login.html";
+  }
+};
+
+await checkAuth();
